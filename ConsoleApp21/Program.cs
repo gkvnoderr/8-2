20b98e8e7371dc1deb4c26e9730037c7cae2a56b
gkvnoderr <123897793+gkvnoderr@users.noreply.github.com>
2023-10-30 20:45:55 +0300

    Add files via upload

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //задание 1
+          //static void Draw()
+          //{
+
+            //    for (int i = 0; i<60;i++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //}
+            //static void Main(string[] args)
+            //{
+            //    Draw();
+            //    Console.ReadKey();
+
+
+            //задание 2
+            //static void Draw()
+            //{
+            //    int h = 0;
+            //    int hh = 2;
+            //    for (int a = 1; a <= 8; a++)
+            //    {
+            //        for (int b = 1; b <= 8; b++)
+            //        {
+            //            if (b == h) Console.Write("#");
+            //            if (b < hh) Console.Write("#");
+            //        }
+            //        Console.Write("\n");
+            //        hh++;
+            //    }
+
+            //}
+            //static void Main(string[] args)
+            //{
+            //    Draw();
+            //    Console.ReadKey();
+            //}
+
+
+            ////задание 3
+            //static void Uravn()
+            //{
+
+            //    double x = ((Math.Sqrt(5)+5)/2)+ ((Math.Sqrt(12) + 12) / 2) + ((Math.Sqrt(19) + 19) / 2) ;
+            //    Console.WriteLine($"{x:f2}");
+            //}
+            //static void Main(string[] args)
+            //{
+            //    Uravn();
+            //    Console.ReadKey();
+            //}
+
+            //задание 4
+            //static void Main(string[] args)
+            //{
+            //    Console.Write("Введите число a: ");
+            //    int a = int.Parse(Console.ReadLine());
+            //    Console.Write("Введите число b: ");
+            //    int b = int.Parse(Console.ReadLine());
+            //    if (a > b)
+            //    { Console.WriteLine($"Число a = {a} максимальное"); }
+            //    else if (a < b)
+            //    { Console.WriteLine($"Число b = {b} максимальное"); }
+            //    else { Console.WriteLine($"Числа a = {a} и b = {b} оба являются макисмальными"); }
+            //    Console.ReadKey();
+            }
+    }
+}
